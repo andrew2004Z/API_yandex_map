2b5890456c6b507c8d970c86acfdd0f4d28ceff7
AMBASSAD0R <44470355+AMBASSAD0R@users.noreply.github.com>
2021-04-19 20:20:28 +0300

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -39,7 +39,7 @@ class Map:
 
 if __name__ == '__main__':
     try:
-        os.mkdir(PATH)
+        os.mkdir('temp/')
     except FileExistsError:
         pass
 
@@ -59,4 +59,4 @@ if __name__ == '__main__':
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
-    shutil.rmtree(PATH, ignore_errors=True)
+    shutil.rmtree('temp/', ignore_errors=True)
