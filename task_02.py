51082bd52b8fd1d4d5e11ef591f9b7fbd025a975
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-19 20:22:24 +0300

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -4,9 +4,7 @@ import requests
 import pygame
 import shutil
 import os
-
-SIZE = (600, 450)
-FPS = 60
+from config import *
 
 
 class Map:
@@ -15,6 +13,7 @@ class Map:
         self.coordinates = coordinates
         self.zoom = zoom
         self.image = self.get_map()
+        self.borders = (0, 17)
 
     def set_params(self, coordinates, zoom):
         self.coordinates = coordinates
@@ -25,16 +24,28 @@ class Map:
 
     def get_map(self):
         map_params = {
-            'l': 'map',
-            'll': ','.join([str(i) for i in self.coordinates]),
-            'z': self.zoom,
+            "l": "map",
+            "ll": ','.join([str(i) for i in self.coordinates]),
+            "z": self.zoom,
         }
-        response = requests.get('http://static-maps.yandex.ru/1.x/', params=map_params)
-        Image.open(BytesIO(response.content)).save('temp/' + 'temp.png')
-        return pygame.image.load('temp/' + 'temp.png')
+        response = requests.get("http://static-maps.yandex.ru/1.x/",
+                                params=map_params)
+        Image.open(BytesIO(response.content)).save(PATH + 'temp.png')
+        return pygame.image.load(PATH + 'temp.png')
 
     def show(self, surf):
         surf.blit(self.image, (0, 0))
+
+    def key_down(self, key):
+        if key not in [pygame.K_PAGEUP, pygame.K_PAGEDOWN]:
+            return
+        if key == pygame.K_PAGEUP:
+            step = 1
+        elif key == pygame.K_PAGEDOWN:
+            step = -1
+        if self.borders[0] <= self.zoom + step <= self.borders[1]:
+            self.zoom += step
+            self.image = self.get_map()
 
 
 if __name__ == '__main__':
@@ -46,15 +57,16 @@ if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
-
     running = True
 
-    operator = Map((50, 50), 5)
+    operator = Map((34.11, 66.56), 5)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                operator.key_down(event.key)
         operator.show(screen)
         pygame.display.flip()
         clock.tick(FPS)
